b246c24e0021e99d6ea996bdf7dfd10fd8f038fb
leov1963 <leov1963@gmail.com>
2021-02-16 11:32:02 -0800

    app.js: Add Rest Timer - fix seconds timer

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,28 @@
 console.log("hello")
 
 let clockSeconds = 00
-let clockMinutes = 15
-let clockMinutesInput = 15
+let clockMinutes = 2
+let clockMinutesInput = 2
+let restMinutes = 2
+let innerTimer = clockMinutesInput + 1
 
 const secondsTimer = () => {
     const sTimer = setInterval(() => {
-        if (clockMinutes === 0) {
+        if (clockSeconds === 0 && clockMinutes === 0) {
             document.querySelector("#time").innerHTML = `${clockMinutes}:0${clockSeconds}`
+            clearInterval(sTimer)
+            restSecondsTimer()
             return
         }
         else if (clockSeconds === 0 && clockMinutes === clockMinutesInput) {
             clockSeconds += 59
             clockMinutes--
+            innerTimer--
             document.querySelector("#time").innerHTML = `${clockMinutes}:${clockSeconds}`
         }
-        else if (clockSeconds === 0) {
+        else if (clockSeconds === 0 && clockMinutes > 0) {
             clockSeconds += 59
+            clockMinutes--
             document.querySelector("#time").innerHTML = `${clockMinutes}:${clockSeconds}`
         }
         else if (clockSeconds >= 11) {
@@ -26,23 +32,46 @@ const secondsTimer = () => {
             clockSeconds--
             document.querySelector("#time").innerHTML = `${clockMinutes}:0${clockSeconds}`
         }
-    }, 1000) 
+    }, 500) 
 }
 
+// const minutesTimer = () => {
+//     const mTimer = setInterval(() => {
+//         if (clockMinutes === 0) {
+//             clearInterval(mTimer)
+//             return
+//         }
+//         else if (clockMinutes > 0) {
+//             clockMinutes--
+//         }
+//     }, 30000) 
+// }
 
-
-const minutesTimer = () => {
-    const mTimer = setInterval(() => {
-        if (clockMinutes === 0) {
+const restSecondsTimer = () => {
+    const RSTimer = setInterval(() => {
+        if (clockMinutes === restMinutes) {
+            document.querySelector("#time").innerHTML = `${clockMinutes}:0${clockSeconds}`
             return
         }
-        else if (clockMinutes > 0) {
-            clockMinutes--
+        else if (clockSeconds === 59) {
+            clockSeconds -= 59
+            clockMinutes++
+            document.querySelector("#time").innerHTML = `${clockMinutes}:0${clockSeconds}`
         }
-    }, 60000) 
+        else if (clockSeconds === 9) {
+            clockSeconds++
+            document.querySelector("#time").innerHTML = `${clockMinutes}:${clockSeconds}`
+        }
+        else if (clockSeconds >= 10) {
+            clockSeconds++
+            document.querySelector("#time").innerHTML = `${clockMinutes}:${clockSeconds}`
+        } 
+        else if (clockSeconds <= 8) {
+            clockSeconds++
+            document.querySelector("#time").innerHTML = `${clockMinutes}:0${clockSeconds}`
+        }
+    }, 500) 
 }
-
-
 
 document.querySelector("#start").addEventListener("click", () => {
     secondsTimer()
