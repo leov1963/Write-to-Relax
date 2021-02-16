e8b3ceb52b075389b7cc126a82aca6200cc0fb04
leov1963 <leov1963@gmail.com>
2021-02-15 20:33:08 -0800

    Add Minutes Timer to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,18 @@ console.log("hello")
 
 let clockSeconds = 00
 let clockMinutes = 15
+let clockMinutesInput = 15
 
 const secondsTimer = () => {
     const sTimer = setInterval(() => {
         if (clockMinutes === 0) {
+            document.querySelector("#time").innerHTML = `${clockMinutes}:0${clockSeconds}`
             return
+        }
+        else if (clockSeconds === 0 && clockMinutes === clockMinutesInput) {
+            clockSeconds += 59
+            clockMinutes--
+            document.querySelector("#time").innerHTML = `${clockMinutes}:${clockSeconds}`
         }
         else if (clockSeconds === 0) {
             clockSeconds += 59
@@ -24,3 +31,15 @@ const secondsTimer = () => {
 
 secondsTimer()
 
+const minutesTimer = () => {
+    const mTimer = setInterval(() => {
+        if (clockMinutes === 0) {
+            return
+        }
+        else if (clockMinutes > 0) {
+            clockMinutes--
+        }
+    }, 60000) 
+}
+
+minutesTimer()
