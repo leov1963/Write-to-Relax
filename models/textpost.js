80650ebeee3a94829a7a6bff91cd4cbc5f7f66b5
leov1963 <leov1963@gmail.com>
2021-02-16 16:51:16 -0800

    textpost.js: Add belongsTo Association for Users

--- models/textpost.js
+++ models/textpost.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.textpost.belongsTo(models.user)
     }
   };
   textpost.init({
