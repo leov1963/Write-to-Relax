ef5c5467a82be7b001124c0bea5bab27c98b93b0
leov1963 <leov1963@gmail.com>
2021-02-16 16:52:38 -0800

    signedin.js: Define textpost

--- controllers/signedin.js
+++ controllers/signedin.js
@@ -3,9 +3,10 @@ const passport = require('../config/ppConfig');
 const router = express.Router();
 
 const db = require('../models');
+const textpost = require('../models/textpost');
 
 router.get('/', (req, res) => {
     res.render('main-page');
-  });
+});
 
-  module.exports = router;
+module.exports = router;
