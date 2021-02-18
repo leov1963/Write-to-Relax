e67653c3107a7ab0dbe8e94a9df7e69b67cea445
leov1963 <leov1963@gmail.com>
2021-02-18 12:17:08 -0800

    signedin.js: Removed unused console logs

--- controllers/signedin.js
+++ controllers/signedin.js
@@ -7,9 +7,6 @@ const db = require('../models');
 const textpost = require('../models/textpost');
 
 router.get('/:zipcode', (req, res) => {
-  console.log(req.params.zipcode)
-  console.log("****************************")
-  console.log(req.body.zipcode)
   const zipcode = req.params.zipcode
   const { id } = req.user.get(); 
   weather.find({search:zipcode, degreeType: "f"}, function(err, result) {
