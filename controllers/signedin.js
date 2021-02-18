897c69e583b0f6bbd5f0aeb0726ac5bff5b89509
leov1963 <leov1963@gmail.com>
2021-02-18 11:50:30 -0800

    signedin.ejs: Fix /signedin GET route

--- controllers/signedin.js
+++ controllers/signedin.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const passport = require('../config/ppConfig');
 const router = express.Router();
+const weather = require('weather-js')
 
 const db = require('../models');
 const textpost = require('../models/textpost');
 
 router.get('/', (req, res) => {
+  console.log(req.params.zipcode)
+  console.log("****************************")
+  console.log(req.body.zipcode)
+  const zipcode = req.params.zipcode
   const { id } = req.user.get(); 
-  res.render('main-page', { id });
+  weather.find({search:zipcode, degreeType: "f"}, function(err, result) {
+    if(err) console.log(err)
+    // const myWeather 
+    console.log(result)
+    const temperature = result[0].current.temperature
+    const skytext = result[0].current.skytext
+    const zipcode = result[0].location.zipcode
+    res.render('main-page', { id, temperature, skytext, zipcode });
+  })
 });
 
 module.exports = router;
